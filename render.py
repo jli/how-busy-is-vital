9d85f9149786eeff50d6af29e73a9450992990f0
john li <jli@circularly.org>
2023-02-06 23:00:27 -0500

    basically working
    - shows data for current day, with historical data for the weekday
    - shows weekday averages with spreads

--- render.py
+++ render.py
@@ -1,75 +1,114 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from typing import cast
 
 import altair as alt
 import pandas as pd
 import streamlit as st
 
-# TODO:
-# draw average for the current day of week (plus the next day maybe)
-# read streamlit docs on how to explore plots (pan, zoom)
+
+def load_busyness() -> tuple[pd.DataFrame, datetime]:
+    """return df indexed by time, column for num_people"""
+    df = pd.read_csv("busyness.csv", names=["time", "num_people"], parse_dates=["time"])
+    last_update = df["time"].iloc[-1]
+    df = df.set_index("time").resample("10min").mean()
+    return cast(pd.DataFrame, df), last_update
 
 
-def split_df_by_day_of_week_and_time(df):
-    # df = df.resample("5T").mean()
-    df = df.copy()
-    df["day_of_week"] = df.index.dayofweek
-    df["day_name"] = df.index.day_name()
-    df["time_of_day"] = df.index.time
-
-    # df_grouped = df.groupby(['day_of_week', 'time_of_day']).mean()
-    weeks = df.index.isocalendar().week.unique()
-    result = pd.DataFrame()
-    for week in weeks:
-        df_week = df[df.index.isocalendar().week == week]
-        df_week_grouped = df_week.groupby(["day_of_week", "day_name", "time_of_day"]).mean()
-        df_week_grouped = df_week_grouped.rename(columns={"num_people": f"week_{week}"})
-        result = pd.concat([result, df_week_grouped], axis=1)
-    result.sort_index(inplace=True)
-
-    # index by strings like "Monday 00:05:00"
-    idx = result.index.to_frame()
-    result.set_index(
-        idx.day_of_week.apply(str) + " " + idx.day_name + " " + idx.time_of_day.apply(str),
-        inplace=True,
+def convert_datetime_column_to_weekday_and_time(series: pd.Series) -> pd.Series:
+    """convert from timestamp series to weekday+time, like "0 Mon 12:30" """
+    return pd.Series(
+        # need this hack because it sorts by x-axis :/
+        series.apply(lambda s: str(s.dayofweek))
+        + " "
+        + series.apply(lambda s: s.day_name()[:3])
+        + " "
+        + series.apply(lambda s: str(s.time()).removesuffix(":00"))
     )
+
+
+def split_df_by_day_of_week_and_time(df: pd.DataFrame) -> pd.DataFrame:
+    """input: time->num_people
+    output: weekday+time -> one column per week"""
+    df = df.copy()
+    df["week_num"] = df.index.isocalendar().week  # type: ignore
+    df["weekday_time"] = convert_datetime_column_to_weekday_and_time(df.index.to_series())
+    weeks = df.week_num.unique()  # all week numbers
+
+    results = []
+    for week in weeks:
+        df_one_week = df[df.week_num == week]
+        df_week_grouped = df_one_week.groupby("weekday_time")[["num_people"]].mean()
+        df_week_grouped = df_week_grouped.rename(columns={"num_people": f"week_{week}"})
+        results.append(df_week_grouped)
+    result = pd.concat(results, axis=1)
+    result.sort_index(inplace=True)
     return result
 
 
-def load_busyness():
-    df = pd.read_csv("busyness.csv", names=["time", "num_people"], parse_dates=["time"])
-    df = df.set_index("time").resample("5min").mean()
-    return df
+def make_spread_df(one_col_per_week_df) -> pd.DataFrame:
+    """input: df indexed by weekday+time, one column per week
+    output: df indexed by weekday+time, columns for mean, std, hi, lo
+    """
+    mean = one_col_per_week_df.mean(axis=1)
+    std = one_col_per_week_df.std(axis=1)
+    hi = mean + std
+    lo = mean - std
+    spread_df = pd.concat([mean, std, hi, lo], keys=["mean", "std", "hi", "lo"], axis=1)
+    return spread_df
+
+
+### state
+
+full_df, last_update = load_busyness()
+one_col_per_week_df = split_df_by_day_of_week_and_time(full_df)
+weekday_spread_df = make_spread_df(one_col_per_week_df)
+
+now = datetime.now()
+today = now.today().date()
+today_dayname = today.strftime("%a")  # 3 char str
+# data for today so far
+today_df = full_df[full_df.index.date == today]
+today_df.index = convert_datetime_column_to_weekday_and_time(today_df.index.to_series())
+
+# chart with vertical rule for current time
+now_df = pd.DataFrame([dict(time=today_df.index[-1])])
+now_chart = alt.Chart(now_df).mark_rule(opacity=0.2).encode(x="time")
+
+
+### display
 
 
 "# how busy is vital?"
 
-df = load_busyness()
+st.write(f"now: {now:%a %m-%d %H:%M}")
+st.write(f"last update: {last_update:%m-%d %H:%M:%S}")
 
-now = datetime.now()
-dt_24h_ago = now - timedelta(days=1)
 
-"## last 24h"
-last_24h_df = df[df.index > dt_24h_ago]  # type: ignore
-st.line_chart(last_24h_df)
+"## right now"
+
+historical_today = weekday_spread_df[lambda df: df.index.str.contains(today_dayname)]
+historical_and_current_today = historical_today.join(
+    today_df.rename(columns=dict(num_people="today"))
+)
+
+# st.line_chart(historical_today)
+x = historical_and_current_today.reset_index(names="time")
+base = alt.Chart(x).encode(x="time")
+chart = (
+    base.mark_bar().encode(y="lo", y2="hi")
+    + base.mark_line().encode(y="mean")
+    + base.mark_line(opacity=1).encode(y="today")
+).configure_mark(opacity=0.3)
+st.altair_chart(chart + now_chart, use_container_width=True)
+
 
 "## weekday averages"
-one_col_per_week = split_df_by_day_of_week_and_time(df)
-st.line_chart(one_col_per_week)
 
-"## weekday averages: mean, stdev"
-mean = one_col_per_week.mean(axis=1).rename("mean")
-std = one_col_per_week.std(axis=1).rename("std")
-x = pd.concat([mean, std], axis=1)
-x["hi"] = x["mean"] + x["std"]
-x["lo"] = x["mean"] - x["std"]
+# st.line_chart(spread_df)
+x = weekday_spread_df.reset_index(names="time")
+base = alt.Chart(x).encode(x="time")
+chart = (
+    base.mark_bar().encode(y="lo", y2="hi") + base.mark_line().encode(y="mean")
+).configure_mark(opacity=0.3)
 
-# TODO: not working yet...
-base = alt.Chart(x.reset_index()).encode(x="index")
-chart = base.mark_line().encode(y="mean") + base.mark_line().encode(y="hi")
-st.altair_chart(chart, use_container_width=True)
-
-# "## full data"
-# st.write(df)
-
-# "## all time"
-# st.line_chart(df.resample("1H").mean())
+st.altair_chart(chart + now_chart, use_container_width=True)
